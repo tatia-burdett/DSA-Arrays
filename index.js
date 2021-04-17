2fd31cdf1ac428f5b8fc796d4b23c79523831b15
Tatia burdett <tatiaburdett@gmail.com>
2021-04-17 14:56:39 -0700

    add class Array code

--- index.js
+++ index.js
@@ -3,15 +3,11 @@ const memory = require('./memory')
 class Array {
   constructor() {
     this.length = 0
-    this._capacity = 0
     this.ptr = memory.allocate(this.length)
   }
 
   push(value) {
-    if (this.length >= this._capacity) {
-      this._resize((this.length + 1) * Array.SIZE_RATIO)
-    }
-
+    this._resize(this.length + 1)
     memory.set(this.ptr + this.length, value)
     this.length++
   }
@@ -24,46 +20,59 @@ class Array {
     }
     memory.copy(this.ptr, oldPtr, this.length)
     memory.free(oldPtr)
-    this._capacity = size
   }
 
   get(index) {
     if (index < 0 || index >= this.length) {
-      throw new Error('Index error')
+        throw new Error('Index error');
     }
-    return memory.get(this.ptr + index)
+    return memory.get(this.ptr + index);
   }
 
   pop() {
     if (this.length == 0) {
-      throw new Error('Index error')
+        throw new Error('Index error');
     }
-    const value = memory.get(this.ptr + this.length - 1)
-    this.length--
-    return value
+    const value = memory.get(this.ptr + this.length - 1);
+    this.length--;
+    return value;
   }
 
   insert(index, value) {
     if (index < 0 || index >= this.length) {
-      throw new Error('Index error')
-    }
-    if (this.length >= this._capacity) {
-      this._resize((this.length + 1) * Array.SIZE_RATIO)
+        throw new Error('Index error');
     }
 
-    memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index)
-    memory.set(this.ptr + index, value)
-    this.length++
+    if (this.length >= this._capacity) {
+        this._resize((this.length + 1) * Array.SIZE_RATIO);
+    }
+
+    memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
+    memory.set(this.ptr + index, value);
+    this.length++;
   }
 
   remove(index) {
     if (index < 0 || index >= this.length) {
-      throw new Error('Index error')
+        throw new Error('Index error');
     }
-    memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1)
-    this.length--
-  }
+    memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1);
+    this.length--;
+  } 
 }
 
 // Array.SIZE_RATIO = 3
+
+function main(){
+
+  Array.SIZE_RATIO = 3;
+
+  // Create an instance of the Array class
+  let arr = new Array();
+
+  // Add an item to the array
+  arr.push(3);
+
+  console.log(arr);
+}
 
